8afdf845082fb0aae874bbf905267cf28cd50d03
Peter <faffyroypeter@gmail.com>
2020-03-12 09:42:20 +0530

    project changes
    Update to

---
+++ LicensePlateReader/Src/LPRDetection/LPRDetection/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LPRDetection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static async Task MakePredictionRequest(string imageFilePath)
+        {
+            var client = new HttpClient();
+
+            // Request headers - replace this example key with your valid Prediction-Key.
+            client.DefaultRequestHeaders.Add("Prediction-Key", "c4733bca242b43cfb9b806dc00da0044");
+
+            // Prediction URL - replace this example URL with your valid Prediction URL.
+            // string url = "<Your prediction URL>";
+            string url = "https://southcentralus.api.cognitive.microsoft.com/subscriptions/68d09ee0-763a-447c-9812-c271bbb7f946/resourceGroups/rg-shc-lpr/providers/Microsoft.CognitiveServices/accounts/CustomVisionShcLPR";
+
+            HttpResponseMessage response;
+
+            // Request body. Try this sample with a locally stored image.
+            byte[] byteData = GetImageAsByteArray(imageFilePath);
+
+            using (var content = new ByteArrayContent(byteData))
+            {
+                content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+                response = await client.PostAsync(url, content);
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        private static byte[] GetImageAsByteArray(string imageFilePath)
+        {
+            FileStream fileStream = new FileStream(imageFilePath, FileMode.Open, FileAccess.Read);
+            BinaryReader binaryReader = new BinaryReader(fileStream);
+            return binaryReader.ReadBytes((int)fileStream.Length);
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            var fileName = openFileDialog1.FileName;
+            await MakePredictionRequest(fileName);
+        }
+    }
+}
